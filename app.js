8f804e98ca7f9e12be3da78263f10da59b0bdcc1
Bishal <bohorabishal20@gmail.com>
2024-03-15 19:03:56 +0545

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
+let carouselDom = document.querySelector('.carousel');
+let listItemDom = document.querySelector('.carousel .list');
+let thumbnailDom = document.querySelector('.carousel .thumbnail');
+
+nextDom.onclick =function(){
+    showSlider('next');
+}
+prevDom.onclick = function(){
+    showSlider('prev');
+}
+
+let timeRunning = 3000;
+let timeAutoNext = 6000;
+let runTimeOut;
+let timeAutoRun = setTimeout(() =>{
+    nextDom.click();
+}, timeAutoNext);
+
+function showSlider(type){
+    let itemSlider = document.querySelectorAll('.carousel .list .item');
+    let itemThumbnail = document.querySelectorAll('.carousel .thumbnail .item');
+
+   if(type=== 'next'){
+    listItemDom.appendChild(itemSlider[0]);
+    thumbnailDom.appendChild(itemThumbnail[0]);
+    carouselDom.classList.add('next');
+   }else{
+    let positionLastImage = itemSlider.length -1;
+    listItemDom.prepend(itemSlider[positionLastImage]);
+    thumbnailDom.prepend(itemThumbnail[positionLastImage]);
+    carouselDom.classList.add('prev');
+   }
+   clearTimeout(runTimeOut);
+   runTimeOut = setTimeout(() =>{
+    carouselDom.classList.remove('next');
+    carouselDom.classList.remove('prev');
+   }, timeRunning)
+
+   clearTimeout(ruunAutoRun);
+   ruunAutoRun = setTimeout(() =>{
+    nextDom.click();
+   }, timeAutoNext);
+
+}
+
+
